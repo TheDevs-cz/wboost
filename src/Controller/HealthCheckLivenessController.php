c7c7ee7606d4f0b605d54285c5872823f150bf0c
Jan Mikeš <j.mikes@me.com>
2024-07-07 12:09:09 +0200

    Healthcheck controller

---
+++ src/Controller/HealthCheckLivenessController.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BrandManuals\Web\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class HealthCheckLivenessController extends AbstractController
+{
+    #[Route(path: '/-/health-check/liveness', methods: ['GET'])]
+    public function __invoke(): Response
+    {
+        return $this->json([
+            'status' => 'ok',
+            'time' => time(),
+        ]);
+    }
+}
