7688aca41868ae000ec29eb25c778f9085a5b4da
Jan Mikeš <j.mikes@me.com>
2024-07-27 00:24:34 +0200

    Add logout route

--- config/routes.php
+++ config/routes.php
@@ -6,4 +6,7 @@ use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 return static function (RoutingConfigurator $routingConfigurator): void {
     $routingConfigurator->import(__DIR__ . '/../src/Controller', 'attribute');
+
+    $routingConfigurator->add('logout', '/logout')
+        ->methods(['GET']);
 };
