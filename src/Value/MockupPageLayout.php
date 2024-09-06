df77d032bb9b62e43bc82b9fe2c7f959274a4505
Jan Mikeš <j.mikes@me.com>
2024-09-06 11:04:50 +0200

    New layout types

--- src/Value/MockupPageLayout.php
+++ src/Value/MockupPageLayout.php
@@ -13,6 +13,10 @@ enum MockupPageLayout: string
     case Layout5 = 'layout-5';
     case Layout6 = 'layout-6';
     case Layout7 = 'layout-7';
+    case Layout8 = 'layout-8';
+    case Layout9 = 'layout-9';
+    case Layout10 = 'layout-10';
+    case Layout11 = 'layout-11';
 
     public function uploadInputsCount(): int
     {
@@ -24,6 +28,10 @@ enum MockupPageLayout: string
             self::Layout5 => 4,
             self::Layout6 => 6,
             self::Layout7 => 1,
+            self::Layout8 => 2,
+            self::Layout9 => 2,
+            self::Layout10 => 3,
+            self::Layout11 => 3,
         };
     }
 }
